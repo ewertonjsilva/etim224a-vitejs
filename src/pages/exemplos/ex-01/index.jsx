e4dcb0c2328adb9003937a3321d4d2032e911db5
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2026-03-10 10:57:29 -0300

    Revert "estilizacao rem"
    This reverts commit 93a899e8f7b9c734b63148a51193b2bac73176dc.

--- src/pages/exemplos/ex-01/index.jsx
+++ src/pages/exemplos/ex-01/index.jsx
@@ -1,9 +1,9 @@
 function Exemplo01() {
     return (
-        <div>
+        <>
             <h1>Exemplo 1</h1>
             <p>Componente básico para renderizar tela com React</p>
-        </div>
+        </>
     )
 }
 
